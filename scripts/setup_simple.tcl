59d8c175d87b811d176b73bfa94685fac28a2951
Jack Freestone <jackcfreestone@gmail.com>
2016-09-22 11:55:12 +0100

    Lab 2 complete

--- scripts/setup_simple.tcl
+++ scripts/setup_simple.tcl
@@ -11,7 +11,7 @@ create_project -force top ./top/ -part xc7z020clg484-1
 
 # Set project properties
 set obj [get_projects top]
-set_property "board_part" xilinx.com:zc702:part0:1.2 $obj
+set_property "board_part" "xilinx.com:zc702:part0:1.2" $obj
 set_property "simulator_language" "Mixed" $obj
 set_property "target_language" "Verilog" $obj
 
